2641c1c0cd717c63afe3dda931ad7b106f13c979
ruhancmendes <ruhancmendes@gmail.com>
2025-09-19 17:16:06 -0300

    register user

--- src/controllers/users-controller.ts
+++ src/controllers/users-controller.ts
@@ -3,6 +3,8 @@
 import { Request, Response } from 'express'
 import { z } from 'zod'
 import { hash } from "bcrypt"
+import { prisma } from "@/database/prisma"
+import { AppError } from '@/utils/AppError'
 
 class UsersController {
     async create(request: Request, response: Response){
@@ -14,9 +16,25 @@ class UsersController {
 
         const { name, email, password } = bodySchema.parse(request.body)
 
+        const userWithSAmeEmail = await prisma.user.findFirst({ where: { email } }) // verifica se já existe um usuário com o mesmo email
+
+        if(userWithSAmeEmail){
+            throw new AppError("Este e-mail já está em uso.")
+        }
+
         const hashedPassword = await hash(password, 8) // criptografa a senha
 
-        return response.status(201).json({ message: "ok!", hashedPassword });
+        const user = await prisma.user.create({
+            data: {
+                name,
+                email,
+                password: hashedPassword,
+            }
+        }) // cria o usuário no banco de dados
+
+        const { password: _, ...userWithoutPassword } = user // remove a senha do usuário para não retornar na resposta
+
+        return response.status(201).json(userWithoutPassword) // retorna o usuário criado;
     }
 }
 
