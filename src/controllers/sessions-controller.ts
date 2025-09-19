276f8fda47db99fd1c607b1803c6b9a191b5cba0
ruhancmendes <ruhancmendes@gmail.com>
2025-09-19 17:40:32 -0300

    recover user

--- src/controllers/sessions-controller.ts
+++ src/controllers/sessions-controller.ts
@@ -1,9 +1,34 @@
 // controller para lidar com a autenticação do usuário / sessão ( login )
 
 import { Request, Response } from "express";
+import { prisma } from "@/database/prisma"
+import { z } from "zod";
+import { AppError } from "@/utils/AppError";
+import { compare } from "bcrypt"
 
 class SessionsController {
-    create(request: Request, response: Response) {
+    async create(request: Request, response: Response) {
+        const bodySchema = z.object({
+            email: z.string().email(),
+            password: z.string().min(6)
+        })// validação dos dados recebidos na requisição
+
+        const { email, password } = bodySchema.parse(request.body) // extrai os dados validados do corpo da requisição
+
+        const user = await prisma.user.findFirst({
+            where: { email }
+        }) // busca o usuário no banco de dados pelo email
+        
+        if(!user) {
+            throw new AppError("E-mail ou senha incorretos", 401)
+        } // se o usuário não for encontrado, lança um erro de autenticação
+
+        const passwordMatched = await compare(password, user.password) // compara a senha fornecida com a senha armazenada no banco de dados
+
+        if(!passwordMatched) {
+            throw new AppError("E-mail ou senha incorretos", 401)
+        } // se as senhas não corresponderem, lança um erro de autenticação
+
         return response.json({ message: "ok!"})
     }
 }
