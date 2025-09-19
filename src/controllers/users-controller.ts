942bd8d98c4e32061c9d1beb75a45fa44571f71c
ruhancmendes <ruhancmendes@gmail.com>
2025-09-19 16:56:29 -0300

    user validation

--- src/controllers/users-controller.ts
+++ src/controllers/users-controller.ts
@@ -1,9 +1,18 @@
 // controller de usuários
 
 import { Request, Response } from 'express'
+import { z } from 'zod'
 
 class UsersController {
     create(request: Request, response: Response){
+        const bodySchema = z.object({
+            name: z.string().trim().min(3),
+            email: z.string().email(),
+            password: z.string().min(6),
+        })
+
+        const { name, email, password } = bodySchema.parse(request.body)
+
         return response.status(201).json({ message: "ok!"});
     }
 }
