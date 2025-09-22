c347d80f79b141ab0d63b14a91e12d31c0a5e420
ruhancmendes <ruhancmendes@gmail.com>
2025-09-22 19:04:17 -0300

    test: register same email

--- src/tests/users-controller.test.ts
+++ src/tests/users-controller.test.ts
@@ -3,6 +3,7 @@
 import request from "supertest"
 import { app } from "@/app"
 import { prisma } from "@/database/prisma"
+import exp from "constants"
 
 describe("UsersController", () => {
     let user_id: string // Variável para armazenar o ID do usuário criado
@@ -23,5 +24,16 @@ describe("UsersController", () => {
         expect(response.body.name).toBe("Test User")
 
         user_id = response.body.id // Armazena o ID do usuário criado
+    })
+
+    it("should throw an error when creating a user with an existing email", async () => {
+        const response = await request(app).post("/users").send({
+            name: "Duplicate User",
+            email: "testuser@example.com",
+            password: "password123",
+        })// Mesma email do teste anterior
+
+        expect(response.status).toBe(400)
+        expect(response.body.message).toBe("Este e-mail já está em uso.")
     })
 })
