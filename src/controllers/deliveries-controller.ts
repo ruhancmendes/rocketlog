81a17ca4f3db15ad02a0bce330892b7fe1bf0296
ruhancmendes <ruhancmendes@gmail.com>
2025-09-22 15:43:23 -0300

    delivery register

--- src/controllers/deliveries-controller.ts
+++ src/controllers/deliveries-controller.ts
@@ -1,10 +1,26 @@
 //controller de entregas
 import { Request, Response } from "express";
+import { prisma } from "@/database/prisma"
+import { z } from "zod";
 
 class DeliveriesController {
-    create(request: Request, response: Response) {
+    async create(request: Request, response: Response) {
         //criar uma entrega
-        return response.json({ message: "Entrega criada" });
+        const bodySchema = z.object({
+          user_id: z.string().uuid(), //id do usuário que será enviado
+          description: z.string(), //descrição da entrega   
+        })
+
+        const { user_id, description } = bodySchema.parse(request.body); //validar o corpo da requisição
+
+        await prisma.delivery.create({
+            data: {
+                userId: user_id,
+                description,
+            }
+        })//criar a entrega no banco de dados
+
+        return response.status(201).json();
     }
 }
 
