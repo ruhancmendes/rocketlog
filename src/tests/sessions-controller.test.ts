9d8b3d66818a9383003915c341db1f4c15dfac1e
ruhancmendes <ruhancmendes@gmail.com>
2025-09-22 19:30:21 -0300

    removing user of authentication

--- src/tests/sessions-controller.test.ts
+++ src/tests/sessions-controller.test.ts
@@ -1,9 +1,14 @@
 // teste automatizado para autenticação
 import request from 'supertest'
 import { app } from '@/app'
+import { prisma } from "@/database/prisma"
 
 describe("SessionsController", () => {
     let user_id: string // Variável para armazenar o ID do usuário criado
+
+    afterAll(async () => {
+        await prisma.user.delete({ where: { id: user_id } }) // Limpa o usuário criado após os testes
+        })
 
     it("should authenticate and get a access token", async () => {
         const userResponse = await request(app).post("/users").send({
