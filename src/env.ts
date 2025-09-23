f9b1f738e3c9a31825c7f48a5f15d1b5d4ddfbb8
ruhancmendes <ruhancmendes@gmail.com>
2025-09-23 17:09:05 -0300

    port config

--- src/env.ts
+++ src/env.ts
@@ -4,7 +4,8 @@ import { z } from "zod";
 
 const envSchema = z.object({
     DATABASE_URL: z.string().url(),
-    JWT_SECRET: z.string().min(1)
+    JWT_SECRET: z.string().min(1),
+    PORT: z.coerce.number().default(3333),
 }) // define o esquema de validação para as variáveis de ambiente
 
 export const env = envSchema.parse(process.env) // valida as variáveis de ambiente e exporta o resultado
